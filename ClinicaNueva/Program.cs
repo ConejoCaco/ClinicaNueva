eade58f8e0db4d18165068b688efcaad0a3ee66c
ConejoCaco <huanca1992mamani@gmail.com>
2023-06-11 19:39:06 -0400

    Creando Carpetas

--- ClinicaNueva/Program.cs
+++ ClinicaNueva/Program.cs
@@ -16,7 +16,7 @@ namespace ClinicaNueva
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form2());
+            Application.Run(new Form1());
         }
     }
 }
